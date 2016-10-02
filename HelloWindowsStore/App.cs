39ec74e32b403e1c959724e7f3db256fd3b0b13e
utkarsh rathor <utkarsh rathor>
2016-10-02 11:45:02 -0700

    Commit in which everything is done using the code . Evrything has been deleted according to the book.I am quoting the sentence from the book verbatim 'Deleting app.xaml , App.xaml.cs , MainPage.xaml ,MainPage.xaml.cs  and the entire Commons folder'

---
+++ HelloWindowsStore/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace HelloWindowsStore
+{
+    public class App :Application
+    {
+        static void Main(string[] args)
+        {
+            Application.Start((p) => new App());
+        }
+
+
+        protected override void OnLaunched(Windows.ApplicationModel.Activation.LaunchActivatedEventArgs args)
+        {
+            TextBlock txtBlk = new TextBlock
+            {
+
+                Text = "Striped Down vresion of the Windows phone 8 application",
+                FontFamily = new FontFamily("Times New Roman"),
+                Foreground = new SolidColorBrush(Colors.Red)
+            };
+
+            Window.Current.Content = txtBlk;
+            Window.Current.Activate();
+            
+        }
+
+    }
+}
